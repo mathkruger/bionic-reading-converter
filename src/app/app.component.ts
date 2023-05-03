a2c0ed6fdaede42cadd847051b3ee8980737ed25
Matheus da Silva Kruger <ms-kruger@hotmail.com>
2023-05-03 09:53:54 -0300

    adding save button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Component } from '@angular/core';
-import { FormatsEnum } from './models/formats.enum';
-import { ConvertionForm } from './models/convertion-form.model';
+import { CommonModule } from "@angular/common";
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Component } from "@angular/core";
+import { FormatsEnum } from "./models/formats.enum";
+import { ConvertionForm } from "./models/convertion-form.model";
 
 @Component({
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   public form: FormGroup<ConvertionForm>;
@@ -26,7 +26,7 @@ export class AppComponent {
     if (!text) return "";
 
     const lines = text.split("\n");
-    
+
     return lines.map(x => {
       const words = x.split(" ").map(y => y.trim());
 
@@ -34,10 +34,10 @@ export class AppComponent {
         if (y.length <= 2) {
           return this.getBoldText(y);
         }
-  
+
         const quantityToHighlight = Math.trunc(y.length / 2);
         const quantityToMaintain = Math.abs(y.length - quantityToHighlight);
-  
+
         return `${this.getBoldText(y.substring(0, quantityToHighlight))}${y.substr(quantityToHighlight, quantityToMaintain)}`;
       }).join(" ");
     }).join("<br />");
@@ -47,6 +47,33 @@ export class AppComponent {
     const { text } = this.form.getRawValue();
     this.results = this.toBionicHTML(text);
     console.log(this.results);
+  }
+
+  public export(id: string) {
+    const element: HTMLElement | null = document.getElementById(id);
+    if (!element) {
+      throw new Error("ID does not exists, impossible to generate PDF file.");
+    }
+    const fileName = prompt("Document title");
+
+    const htmlToSave = `
+      <html>
+        <head>
+          <title>${fileName}</title>
+          <style>
+            font-weight: 800;
+          </style>
+        </head>
+        <body>
+          ${element.innerHTML}
+        </body>
+      </html>
+    `;
+
+    const link = document.createElement('a');
+    link.setAttribute('href', 'data:text/html;charset=utf-8,' + encodeURIComponent(htmlToSave));
+    link.setAttribute('download', (fileName || "results") + ".html");
+    link.click();
   }
 
   private getBoldText(text: string) {
